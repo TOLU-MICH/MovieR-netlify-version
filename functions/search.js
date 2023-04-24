a44cbbe26487e7f91936e07a9c47acc272b29e2b
TOLU-MICH <michealtolu2003@gmail.com>
2023-04-24 19:31:23 +0100

    created a serverless function for searching.

---
+++ functions/search.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
+
+exports.handler = async function (event, context) {
+  const data = JSON.parse(event.body);
+  // get the search result for the item specified in the search bar
+  const search = await fetch(
+    `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${data.value}`
+  );
+  const searchJson = await search.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(searchJson.results),
+  };
+};
