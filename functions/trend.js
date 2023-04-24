1f08ec4f733f680ae4231c6f85dcbb801fa82a34
TOLU-MICH <michealtolu2003@gmail.com>
2023-04-24 19:36:56 +0100

    created a serverless function for trends.

---
+++ functions/trend.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
+
+exports.handler = async function (event, context) {
+  // get the trending tv series for the present day
+  const trendingSeries = await fetch(
+    `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`
+  );
+  // get the trending movies for the present day
+  const trendingMovie = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
+  );
+  const seriesJson = await trendingSeries.json();
+  const movieJson = await trendingMovie.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      series: seriesJson.results,
+      movie: movieJson.results,
+    }),
+  };
+};
