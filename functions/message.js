31f738eef4a79ade89352e1edbecffce94b5b8ca
TOLU-MICH <michealtolu2003@gmail.com>
2023-04-29 00:40:05 +0100

    commented fetch.

--- functions/message.js
+++ functions/message.js
@@ -1,8 +1,10 @@
 const apiKey = process.env.API_KEY;
 
 exports.handler = async function (event, context) {
+  console.log(typeof apiKey);
+  const response = require("../thing.json");
   return {
     statusCode: 200,
-    body: JSON.stringify(apiKey),
+    body: response,
   };
 };
