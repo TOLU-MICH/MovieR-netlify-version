b47336c38ec6ffd05c3642b2c8a8a24b36c20538
TOLU-MICH <michealtolu2003@gmail.com>
2023-04-29 01:17:05 +0100

    m

--- functions/message.js
+++ functions/message.js
@@ -2,9 +2,8 @@ const apiKey = process.env.API_KEY;
 
 exports.handler = async function (event, context) {
   console.log(typeof apiKey);
-  const response = require("../thing.json");
   return {
     statusCode: 200,
-    body: response,
+    body: "apiKey",
   };
 };
--- functions/movie.js
+++ functions/movie.js
@@ -1,25 +1,13 @@
 // require("dotenv").config();
 const apiKey = process.env.API_KEY;
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 exports.handler = async function (event, context) {
-  // get the trendig movies and tv series for the present day
-  // const trending = await fetch(
-  //   `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
-  //   );
 
-  // get the popular movies and tv series f
-  //   const popular = await fetch(
-  //   `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
-  // );
-
-  // const trendJson = await trending.json();
-  // const popularJson = await popular.json();
   return {
     statusCode: 200,
     body: JSON.stringify({
-      // trending: trendJson.results,
-      // popular: popularJson.results,
+      apiKey
     }),
   };
 };
